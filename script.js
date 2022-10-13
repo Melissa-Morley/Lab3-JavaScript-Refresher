bc782fe766bea74c54c48dc321cd4e0c86d87bde
Melissa Morley <melissarose.morley@gmail.com>
2022-10-13 16:07:16 -0400

    second commit

--- script.js
+++ script.js
@@ -9,7 +9,7 @@ console.log(population);
 console.log(population * 3);
 
 let javaScriptIsCool = true;
-let colors = ("red", "orange", "yellow", "green", "blue", "purple");
+let colors = ["red", "orange", "yellow", "green", "blue", "purple"];
 
 for (let i = 1; i <= 5; i++) {
    console.log(i) 
