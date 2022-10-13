28893d867206e17876279ce99904891396f1b0f5
Melissa Morley <melissarose.morley@gmail.com>
2022-10-13 15:51:08 -0400

    first commit

---
+++ script.js
@@ -0,0 +1,20 @@
+let firstName= "Melissa";
+let lastName= "Morley";
+let fullName= firstName + " " + lastName;
+console.log(fullName);   
+
+let population = 672351;
+console.log(population);
+
+console.log(population * 3);
+
+let javaScriptIsCool = true;
+let colors = ("red", "orange", "yellow", "green", "blue", "purple");
+
+for (let i = 1; i <= 5; i++) {
+   console.log(i) 
+}
+
+for (let i = 0; i <= 4; i++) {
+   console.log(i)
+}
